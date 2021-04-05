0fc2381c24038f81cbafaaeb3e01b0a73b46f003
deepak <deepakjha6210@gmail.com>
2021-04-05 10:39:14 +0530

    initial commit

---
+++ router1/src/components/header/Nav.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+export class Nav extends Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li><Link to='/'>Home</Link></li>
+                    <li><Link to='/blog'>Blog</Link></li>
+                    <li><Link to='/contact'>Contact</Link></li>
+                    <li><Link to='/about'>About</Link></li>
+                    <li><Link to='/login'>Login / Register</Link></li>
+                    <li className='close'>X</li>
+                </ul>
+                <div className='menu'>Menu</div>
+            </div>
+        )
+    }
+}
+
+export default Nav;
